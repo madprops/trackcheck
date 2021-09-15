61948b65878ff146d1517f1b02d0997ed7b05186
Auric Vente <mprops@protonmail.com>
2021-09-15 12:10:44 -0600

    Update

--- base.js
+++ base.js
@@ -2,7 +2,6 @@ const TC = {}
 TC.playing = 0
 TC.last_pos = 0
 TC.max_tracks = 8
-TC.tracks = {}
 
 TC.init = function () {
   TC.create_tracks()
@@ -26,8 +25,32 @@ TC.create_tracks = function () {
     play.classList.add("button")
     play.textContent = "Play"
     play.addEventListener("click", function () {
-      TC.play(i)
+      TC.play(TC.get_track_index(this.parentNode))
     })
+
+    let up = document.createElement("div")
+    up.classList.add("track_up")
+    up.classList.add("button")
+    up.textContent = "Up"
+    up.addEventListener("click", function () {
+      TC.go_up(TC.get_track_index(this.parentNode))
+    })
+    
+    let down = document.createElement("div")
+    down.classList.add("track_down")
+    down.classList.add("button")
+    down.textContent = "Down"
+    down.addEventListener("click", function () {
+      TC.go_down(TC.get_track_index(this.parentNode))
+    })
+    
+    let remove = document.createElement("div")
+    remove.classList.add("track_remove")
+    remove.classList.add("button")
+    remove.textContent = "Remove"
+    remove.addEventListener("click", function () {
+      TC.remove_track_file(TC.get_track_index(this.parentNode))
+    })     
 
     let file = document.createElement("input")
     file.type = "file"
@@ -44,52 +67,48 @@ TC.create_tracks = function () {
     
     container.appendChild(number)
     container.appendChild(play)
+    container.appendChild(up)
+    container.appendChild(down)
+    container.appendChild(remove)
     container.appendChild(file)
     container.appendChild(audio)
     tracks.appendChild(container)
-    TC.tracks[i] = container
   }
 }
 
 TC.play = function (i) {
-  let track = TC.tracks[i]
+  let track = TC.get_track(i)
   let audio = track.querySelector(".audio")
   let fileinput = track.querySelector(".track_file")
-  let playbutton = track.querySelector(".track_play")
 
-  if (i == TC.playing) {
-    playbutton.classList.remove("button_active")
+  if (i === TC.playing) {
     TC.playing = 0
     TC.last_pos = audio.currentTime
     audio.pause()
+    TC.highlight_play()
     return
   }
 
-  var files = fileinput.files
+  let files = fileinput.files
   
   if (files.length === 0) {
     return
   }
 
-  var path = URL.createObjectURL(files[0])
+  let path = URL.createObjectURL(files[0])
   TC.pause_all() 
   
   audio.src = path
   audio.currentTime = TC.get_current_pos()
   audio.play()
   TC.playing = i
-  
-  let buttons = Array.from(document.querySelectorAll(".track_play"))
-  for (button of buttons) {
-    button.classList.remove("button_active")
-  }
 
-  playbutton.classList.add("button_active")
+  TC.highlight_play()
 }
 
 TC.pause_all = function () {
   let audios = Array.from(document.querySelectorAll(".audio"))
-  for (au of audios) {
+  for (let au of audios) {
     au.pause()
   }
 }
@@ -99,7 +118,7 @@ TC.get_current_pos = function () {
     return TC.last_pos
   }
   
-  let track = TC.tracks[TC.playing]
+  let track = TC.get_track(TC.playing)
   let audio = track.querySelector(".audio")
   return audio.currentTime
 }
@@ -146,7 +165,7 @@ TC.goto_pos_by_percentage = function (percentage) {
 }
 
 TC.get_current_audio = function () {
-  return TC.tracks[TC.playing].querySelector(".audio")
+  return TC.get_track(TC.playing).querySelector(".audio")
 }
 
 TC.start_controls = function () {
@@ -179,4 +198,96 @@ TC.start_controls = function () {
     audio.currentTime = audio.currentTime + 5
     audio.play()
   })  
+}
+
+TC.go_up = function (i) {
+  if (i <= 1) {
+    return
+  }
+
+  TC.move_track(TC.get_track(i), "up")
+
+  if (TC.playing === i) {
+    TC.playing = i - 1
+  } else if (TC.playing === i - 1) {
+    TC.playing = i
+  }
+
+  TC.highlight_play()
+  TC.update_track_number()
+}
+
+TC.go_down = function (i) {
+  if (i >= TC.max_tracks) {
+    return
+  }
+  
+  TC.move_track(TC.get_track(i), "down")
+
+  if (TC.playing === i) {
+    TC.playing = i + 1
+  } else if (TC.playing === i + 1) {
+    TC.playing = i
+  }
+
+  TC.highlight_play()
+  TC.update_track_number()
+}
+
+TC.move_track = function (elem, direction) {
+  let parent = elem.parentNode
+
+  if (direction === "up" && elem.previousElementSibling) {
+    parent.insertBefore(elem, elem.previousElementSibling)
+  } else if (direction === "down" && elem.nextElementSibling) {
+    parent.insertBefore(elem, elem.nextElementSibling.nextElementSibling)
+  }
+}
+
+TC.remove_track_file = function (i) {
+  if (confirm("Are you sure?")) {
+    let input = TC.get_track(i).querySelector(".track_file")
+    input.value = []
+  }
+}
+
+TC.highlight_play = function () {
+  let buttons = Array.from(document.querySelectorAll(".track_play"))
+  for (let button of buttons) {
+    button.classList.remove("button_active")
+  }
+  
+  if (TC.playing > 0) {
+    let playbutton = TC.get_track(TC.playing).querySelector(".track_play")
+    playbutton.classList.add("button_active")
+  }
+}
+
+TC.get_tracks = function () {
+  return Array.from(document.querySelectorAll(".track_container"))
+}
+
+TC.get_track = function (i) {
+  let tracks = TC.get_tracks()
+  return tracks[i - 1]
+}
+
+TC.get_track_index = function (container) {
+  let tracks = TC.get_tracks()
+  let i = 1
+  for (let track of tracks) {
+    if (track === container) {
+      return i
+    }
+    i += 1
+  }
+}
+
+TC.update_track_number = function () {
+  let i = 1
+  for (let track of TC.get_tracks()) {
+    let number = track.querySelector(".track_number")
+    number.textContent = `(${i})`
+    i += 1
+  }
 }
