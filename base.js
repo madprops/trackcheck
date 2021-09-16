4ea4eab23df4785cf7dca9293fce4e9e82a22241
Auric Vente <mprops@protonmail.com>
2021-09-16 04:57:38 -0600

    Info

--- base.js
+++ base.js
@@ -341,7 +341,7 @@ For instance if you have two versions/renders of a track
 And you want to check subtle differences
 You can quickly switch between them
 While keeping the progress (time) synched
-The marker above the slider is for you to remember points`
+The markers above the slider are for you to remember points`
   alert(info)
 }
 
