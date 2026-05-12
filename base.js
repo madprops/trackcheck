ee2173dfe09a977b368ac157535eb61b1146fca5
madprops <easywest@protonmail.com>
2026-05-11 19:56:09 -0600

    Mods

--- base.js
+++ base.js
@@ -1,5 +1,4 @@
 let TC = {}
-
 TC.playing = 0
 TC.last_playing = 0
 TC.last_pos = 0
@@ -27,10 +26,8 @@ TC.add_track = () => {
   let tracks = document.getElementById(`tracks`)
   let container = document.createElement(`div`)
   container.classList.add(`track_container`)
-
   let number = document.createElement(`div`)
   number.classList.add(`track_number`)
-
   let play = document.createElement(`div`)
   play.classList.add(`track_play`)
   play.classList.add(`button_disabled`)
@@ -137,7 +134,7 @@ TC.play = (i, current_time = -1) => {
   let track = TC.get_track(i)
   let audio = track.querySelector(`.audio`)
 
-  if ((i === TC.playing) && (current_time === -1)) {
+  if (i === TC.playing && current_time === -1) {
     TC.last_playing = TC.playing
     TC.playing = 0
     TC.last_pos = audio.currentTime
@@ -146,14 +143,11 @@ TC.play = (i, current_time = -1) => {
     return
   }
 
-  let path = TC.get_audio_path(i)
-
-  if (!path) {
+  if (!TC.track_loaded(track)) {
     return
   }
 
   TC.pause_all()
-  audio.src = path
 
   if (current_time === -1) {
     audio.currentTime = TC.get_current_pos()
@@ -162,13 +156,12 @@ TC.play = (i, current_time = -1) => {
     audio.currentTime = current_time
   }
 
-  audio.play()
+  audio.play().catch(e => console.error(e))
   TC.playing = i
-
   TC.highlight_play()
 }
 
-TC.get_audio_path = (i) => {
+TC.get_audio_path = i => {
   let track = TC.get_track(i)
   let fileinput = track.querySelector(`.track_file`)
   let files = fileinput.files
@@ -198,77 +191,79 @@ TC.get_current_pos = () => {
   return audio.currentTime
 }
 
-TC.format_time = (n) => {
+TC.format_time = n => {
   let seconds = parseInt(n)
   return new Date(seconds * 1000).toISOString().substr(11, 8)
 }
 
-TC.after_audio_starts = (audio) => {
+TC.after_audio_starts = audio => {
   let total = document.querySelector(`#progress_total`)
   total.textContent = TC.format_time(audio.duration)
 }
 
-TC.after_audio_plays = (audio) => {
+TC.after_audio_plays = audio => {
   let current = document.querySelector(`#progress_current`)
   current.textContent = TC.format_time(audio.currentTime)
   TC.update_progressbar()
 }
 
-TC.after_audio_ends = (audio) => {
+TC.after_audio_ends = () => {
   TC.restart()
 }
 
 TC.start_progressbar = () => {
-  let prog = document.querySelector(`#progressbar`)
-  prog.value = 0
+  let canvas = document.querySelector(`#main_waveform`)
 
-  prog.addEventListener(`change`, () => {
-    TC.goto_pos_by_percentage(prog.value)
-  })
-
-  prog.addEventListener(`input`, () => {
-    TC.show_progressbar_preview(prog)
-  })
-
-  prog.addEventListener(`mousedown`, () => {
+  canvas.addEventListener(`mousedown`, e => {
     TC.prog_mouse_down = true
+    TC.handle_waveform_click(e)
   })
 
-  prog.addEventListener(`mouseup`, () => {
+  canvas.addEventListener(`mousemove`, e => {
+    if (TC.prog_mouse_down) {
+      TC.handle_waveform_click(e)
+    }
+
+    TC.show_progressbar_preview(e)
+  })
+
+  window.addEventListener(`mouseup`, () => {
     TC.prog_mouse_down = false
     let preview = document.querySelector(`#progress_preview`)
     preview.textContent = ``
   })
 }
 
+TC.handle_waveform_click = e => {
+  let canvas = document.querySelector(`#main_waveform`)
+  let rect = canvas.getBoundingClientRect()
+  let x = e.clientX - rect.left
+  let percentage = x / canvas.width * 100
+  TC.goto_pos_by_percentage(percentage)
+}
+
 TC.update_progressbar = () => {
-  if ((TC.playing === 0) || TC.prog_mouse_down) {
+  if (TC.playing === 0 || TC.prog_mouse_down) {
     return
   }
 
-  TC.set_progressbar(TC.get_percentage())
+  TC.render_main_waveform()
 }
 
 TC.get_percentage = () => {
   let [audio, i] = TC.get_proper_audio()
-  return (audio.currentTime / audio.duration) * 100
+  return audio.currentTime / audio.duration * 100
 }
 
-TC.get_pb_percentage = () => {
-  let prog = document.querySelector(`#progressbar`)
-  return prog.value
-}
-
-TC.set_progressbar = (percentage) => {
+TC.set_progressbar = percentage => {
   if (isNaN(percentage)) {
     return
   }
 
-  let prog = document.querySelector(`#progressbar`)
-  prog.value = percentage
+  TC.render_main_waveform()
 }
 
-TC.goto_pos_by_percentage = (percentage) => {
+TC.goto_pos_by_percentage = percentage => {
   let [audio, i] = TC.get_proper_audio()
 
   if (!audio) {
@@ -276,7 +271,7 @@ TC.goto_pos_by_percentage = (percentage) => {
     return
   }
 
-  let seconds = (percentage / 100) * audio.duration
+  let seconds = percentage / 100 * audio.duration
 
   if (i !== TC.playing) {
     TC.play(i, seconds)
@@ -284,18 +279,23 @@ TC.goto_pos_by_percentage = (percentage) => {
   else {
     audio.currentTime = seconds
   }
+
+  TC.render_main_waveform()
 }
 
-TC.show_progressbar_preview = (pb) => {
+TC.show_progressbar_preview = e => {
   let [audio, i] = TC.get_proper_audio()
 
   if (!audio) {
-    TC.set_progressbar(0)
     return
   }
 
+  let canvas = document.querySelector(`#main_waveform`)
+  let rect = canvas.getBoundingClientRect()
+  let x = e.clientX - rect.left
+  let percentage = x / canvas.width
+  let seconds = percentage * audio.duration
   let preview = document.querySelector(`#progress_preview`)
-  let seconds = (pb.value / 100) * audio.duration
   preview.textContent = `(${TC.format_time(seconds)})`
 }
 
@@ -359,7 +359,7 @@ TC.start_controls = () => {
   })
 }
 
-TC.go_up = (i) => {
+TC.go_up = i => {
   let dir = `up`
 
   if (i <= 1) {
@@ -386,7 +386,7 @@ TC.go_up = (i) => {
   TC.update_track_number()
 }
 
-TC.go_down = (i) => {
+TC.go_down = i => {
   let dir = `down`
 
   if (i >= TC.get_tracks().length) {
@@ -416,21 +416,21 @@ TC.go_down = (i) => {
 TC.move_track = (elem, dir) => {
   let parent = elem.parentNode
 
-  if ((dir === `up`) && elem.previousElementSibling) {
+  if (dir === `up` && elem.previousElementSibling) {
     parent.insertBefore(elem, elem.previousElementSibling)
   }
-  else if ((dir === `down`) && elem.nextElementSibling) {
+  else if (dir === `down` && elem.nextElementSibling) {
     parent.insertBefore(elem, elem.nextElementSibling.nextElementSibling)
   }
-  else if ((dir === `wrap_down`) && elem.nextElementSibling) {
+  else if (dir === `wrap_down` && elem.nextElementSibling) {
     parent.appendChild(elem)
   }
-  else if ((dir === `wrap_up`) && elem.previousElementSibling) {
+  else if (dir === `wrap_up` && elem.previousElementSibling) {
     parent.prepend(elem)
   }
 }
 
-TC.clear_track_file = (i) => {
+TC.clear_track_file = i => {
   let track = TC.get_track(i)
 
   if (!TC.track_loaded(track) || confirm(`Are you sure?`)) {
@@ -441,8 +441,10 @@ TC.clear_track_file = (i) => {
 
     let input = track.querySelector(`.track_file`)
     input.value = ``
+    track.waveform_data = null
     TC.disable_play_button(track)
     TC.check_if_loaded()
+    TC.render_main_waveform()
   }
 }
 
@@ -461,18 +463,20 @@ TC.highlight_play = () => {
     playbutton.classList.add(`button_active`)
     active_track.classList.add(`track_active`)
   }
+
+  TC.render_main_waveform()
 }
 
 TC.get_tracks = () => {
   return Array.from(document.querySelectorAll(`.track_container`))
 }
 
-TC.get_track = (i) => {
+TC.get_track = i => {
   let tracks = TC.get_tracks()
   return tracks[i - 1]
 }
 
-TC.get_track_index = (container) => {
+TC.get_track_index = container => {
   let tracks = TC.get_tracks()
   let i = 1
 
@@ -508,7 +512,7 @@ TC.start_marks = () => {
     width += TC.mark_width
   }
 
-  marks.addEventListener(`mousedown`, (e) => {
+  marks.addEventListener(`mousedown`, e => {
     TC.toggle_mark(e.target)
   })
 }
@@ -525,29 +529,58 @@ The markers above the slider are for you to remember points`
 
 TC.toggle_blind_mode = () => {
   let body = document.querySelector(`body`)
-  body.classList.toggle(`blind_mode`)
   let tracks_container = document.querySelector(`#tracks`)
-  let tracks = TC.get_tracks()
+  let all_tracks = TC.get_tracks()
+  let loaded_tracks = all_tracks.filter(t => TC.track_loaded(t))
+  let unloaded_tracks = all_tracks.filter(t => !TC.track_loaded(t))
   let active_track = null
+  let current_pos = TC.get_current_pos()
 
   if (TC.playing > 0) {
     active_track = TC.get_track(TC.playing)
   }
 
-  for (let i = (tracks.length - 1); i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1))
-    tracks_container.insertBefore(tracks[j], tracks[i])
-  }
+  body.classList.toggle(`blind_mode`)
 
-  if (active_track) {
-    TC.playing = TC.get_track_index(active_track)
-  }
+  if (body.classList.contains(`blind_mode`)) {
+    for (let i = loaded_tracks.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1))
+      let temp = loaded_tracks[i]
+      loaded_tracks[i] = loaded_tracks[j]
+      loaded_tracks[j] = temp
+    }
 
-  TC.update_track_number()
-  TC.highlight_play()
+    for (let track of loaded_tracks) {
+      tracks_container.appendChild(track)
+    }
+
+    for (let track of unloaded_tracks) {
+      tracks_container.appendChild(track)
+    }
+
+    TC.update_track_number()
+
+    if (loaded_tracks.length > 0) {
+      let random_idx = Math.floor(Math.random() * loaded_tracks.length)
+      let random_track = loaded_tracks[random_idx]
+      TC.playing = 0
+      TC.play(TC.get_track_index(random_track), current_pos)
+    }
+  }
+  else {
+    TC.update_track_number()
+
+    if (active_track) {
+      TC.playing = 0
+      TC.play(TC.get_track_index(active_track), current_pos)
+    }
+    else {
+      TC.highlight_play()
+    }
+  }
 }
 
-TC.toggle_mark = (mark) => {
+TC.toggle_mark = mark => {
   if (mark.classList.contains(`mark`)) {
     if (mark.classList.contains(`active_mark`)) {
       mark.classList.remove(`active_mark`)
@@ -561,14 +594,14 @@ TC.toggle_mark = (mark) => {
 TC.unmark = () => {
   let marks = Array.from(document.querySelectorAll(`.active_mark`))
 
-  if ((marks.length > 0) && confirm(`Are you sure?`)) {
+  if (marks.length > 0 && confirm(`Are you sure?`)) {
     for (let mark of marks) {
       mark.classList.remove(`active_mark`)
     }
   }
 }
 
-TC.remove_track = (track) => {
+TC.remove_track = track => {
   if (TC.get_tracks().length === 1) {
     return
   }
@@ -585,6 +618,7 @@ TC.remove_track = (track) => {
     track.parentNode.removeChild(track)
     TC.update_track_number()
     TC.check_if_loaded()
+    TC.render_main_waveform()
   }
 }
 
@@ -603,34 +637,120 @@ TC.restart = () => {
     audio.play()
   }
 
-  TC.set_progressbar(0)
+  TC.render_main_waveform()
 }
 
-TC.enable_play_button = (track) => {
+TC.enable_play_button = track => {
   let play = track.querySelector(`.track_play`)
   play.classList.remove(`button_disabled`)
 }
 
-TC.disable_play_button = (track) => {
+TC.disable_play_button = track => {
   let play = track.querySelector(`.track_play`)
   play.classList.add(`button_disabled`)
 }
 
-TC.after_file_change = (fileinput) => {
+TC.after_file_change = fileinput => {
   let parent = fileinput.parentNode
 
   if (fileinput.value) {
     TC.enable_play_button(parent)
+    TC.process_audio_file(parent, fileinput.files[0])
   }
   else {
     TC.disable_play_button(parent)
+    parent.waveform_data = null
+    TC.render_main_waveform()
   }
 
   let audio = parent.querySelector(`.audio`)
   let i = TC.get_track_index(parent)
   audio.currentTime = 0
-  audio.src = TC.get_audio_path(i)
-  TC.play(i, 0)
+
+  if (fileinput.files.length > 0) {
+    audio.src = URL.createObjectURL(fileinput.files[0])
+    TC.play(i, 0)
+  }
+  else {
+    audio.src = ``
+    TC.pause_all()
+    TC.playing = 0
+    TC.highlight_play()
+  }
+}
+
+TC.process_audio_file = async (track, file) => {
+  let array_buffer = await file.arrayBuffer()
+  let audio_ctx = new (window.AudioContext || window.webkitAudioContext)()
+  let audio_buffer = await audio_ctx.decodeAudioData(array_buffer)
+  let raw_data = audio_buffer.getChannelData(0)
+  let samples = 620
+  let block_size = Math.floor(raw_data.length / samples)
+  let filtered_data = []
+
+  for (let i = 0; i < samples; i++) {
+    let block_start = block_size * i
+    let sum = 0
+
+    for (let j = 0; j < block_size; j++) {
+      sum = sum + Math.abs(raw_data[block_start + j])
+    }
+
+    filtered_data.push(sum / block_size)
+  }
+
+  let max_val = Math.max(...filtered_data)
+  let multiplier = 1 / max_val
+  let normalized = filtered_data.map(n => n * multiplier)
+  track.waveform_data = normalized
+  TC.render_main_waveform()
+}
+
+TC.render_main_waveform = () => {
+  let canvas = document.querySelector(`#main_waveform`)
+
+  if (!canvas) {
+    return
+  }
+
+  let ctx = canvas.getContext(`2d`)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  let [audio, i] = TC.get_proper_audio()
+
+  if (!audio || i === 0) {
+    return
+  }
+
+  let track = TC.get_track(i)
+  let data = track.waveform_data
+
+  if (!data) {
+    return
+  }
+
+  let percentage = 0
+
+  if (audio.duration > 0) {
+    percentage = audio.currentTime / audio.duration
+  }
+
+  let split_x = canvas.width * percentage
+  let samples = data.length
+
+  for (let j = 0; j < samples; j++) {
+    let x = canvas.width * (j / samples)
+    let height = data[j] * canvas.height
+    let y = canvas.height / 2 - height / 2
+
+    if (x <= split_x) {
+      ctx.fillStyle = `#ee7575`
+    }
+    else {
+      ctx.fillStyle = `#3daee9`
+    }
+
+    ctx.fillRect(x, y, canvas.width / samples, height)
+  }
 }
 
 TC.get_first_loaded_track = () => {
@@ -648,13 +768,13 @@ TC.get_first_loaded_track = () => {
   return 0
 }
 
-TC.track_loaded = (track) => {
+TC.track_loaded = track => {
   let fileinput = track.querySelector(`.track_file`)
   return fileinput.files.length > 0
 }
 
 TC.start_key_detection = () => {
-  document.addEventListener(`keydown`, (e) => {
+  document.addEventListener(`keydown`, e => {
     if (e.key === ` `) {
       TC.play(0)
       e.preventDefault()
@@ -705,6 +825,103 @@ TC.go_back = () => {
   else {
     audio.currentTime = seconds
   }
+
+  TC.render_main_waveform()
 }
 
-TC.go_forward
+TC.go_forward = () => {
+  let [audio, i] = TC.get_proper_audio()
+
+  if (!audio) {
+    return
+  }
+
+  let seconds = audio.currentTime + 5
+
+  if (i !== TC.playing) {
+    TC.play(i, seconds)
+  }
+  else {
+    audio.currentTime = seconds
+  }
+
+  TC.render_main_waveform()
+}
+
+TC.goto_prev_track = () => {
+  let [audio, i] = TC.get_proper_audio()
+  let tracks = TC.get_tracks()
+
+  for (let i2 = tracks.length - 1; i2 >= 0; i2--) {
+    if (i2 < i - 1) {
+      if (TC.track_loaded(tracks[i2])) {
+        TC.play(i2 + 1)
+        return
+      }
+    }
+  }
+
+  for (let i2 = tracks.length - 1; i2 >= 0; i2--) {
+    if (i2 !== i - 1) {
+      if (TC.track_loaded(tracks[i2])) {
+        TC.play(i2 + 1)
+        return
+      }
+    }
+  }
+}
+
+TC.goto_next_track = () => {
+  let [audio, i] = TC.get_proper_audio()
+  let tracks = TC.get_tracks()
+
+  for (let i2 = 0; i2 < tracks.length; i2++) {
+    if (i2 > i - 1) {
+      if (TC.track_loaded(tracks[i2])) {
+        TC.play(i2 + 1)
+        return
+      }
+    }
+  }
+
+  for (let i2 = 0; i2 < tracks.length; i2++) {
+    if (TC.track_loaded(tracks[i2])) {
+      if (i2 !== i - 1) {
+        TC.play(i2 + 1)
+        return
+      }
+    }
+  }
+
+  TC.goto_prev_track()
+}
+
+TC.check_if_loaded = () => {
+  let none_active = true
+
+  for (let t of TC.get_tracks()) {
+    if (TC.track_loaded(t)) {
+      none_active = false
+      break
+    }
+  }
+
+  if (none_active) {
+    TC.last_playing = 0
+    let canvas = document.querySelector(`#main_waveform`)
+
+    if (canvas) {
+      let ctx = canvas.getContext(`2d`)
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+    }
+
+    TC.reset_times()
+  }
+}
+
+TC.reset_times = () => {
+  let total = document.querySelector(`#progress_total`)
+  total.textContent = TC.format_time(0)
+  let current = document.querySelector(`#progress_current`)
+  current.textContent = TC.format_time(0)
+}
