d4c97745366fa52bbcab6473aa0c2d947dc2343f
madprops <easywest@protonmail.com>
2026-05-11 20:10:09 -0600

    Mods

--- base.js
+++ base.js
@@ -526,7 +526,7 @@ For instance if you have two versions/renders of a track.
 And you want to check subtle differences.
 You can quickly switch between them.
 While keeping the progress (time) synched.
-The markers above the slider are for you to remember points.`
+The markers above the waveform are for you to remember points.`
   alert(info)
 }
 
