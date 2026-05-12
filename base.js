af1dd099a12413505734c051527f1312025394fd
madprops <easywest@protonmail.com>
2026-05-11 20:00:18 -0600

    Mods

--- base.js
+++ base.js
@@ -518,12 +518,12 @@ TC.start_marks = () => {
 }
 
 TC.show_info = () => {
-  let info = `The purpose of this is to compare similar tracks
-For instance if you have two versions/renders of a track
-And you want to check subtle differences
-You can quickly switch between them
-While keeping the progress (time) synched
-The markers above the slider are for you to remember points`
+  let info = `The purpose of this is to compare similar tracks.
+For instance if you have two versions/renders of a track.
+And you want to check subtle differences.
+You can quickly switch between them.
+While keeping the progress (time) synched.
+The markers above the slider are for you to remember points.`
   alert(info)
 }
 
